645dd7a3b7d4f62846ecd040a0e3c877775f6e2d
YuPo13 <yuliya.povzlo@gmail.com>
2020-06-22 14:49:59 +0300

    logic test-5

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from pyppeteer import launch
 
 app = Flask(__name__)
 bot = telegram.Bot(token=TOKEN)
-image = ""
+
+
 
 async def make_screenshot(url):
     browser = await launch(headless=True)
@@ -16,7 +17,7 @@ async def make_screenshot(url):
     await page.goto(url)
     image = await page.screenshot({'path': 'screen.png', 'fullPage': True})
     await browser.close()
-
+    return image
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
@@ -50,9 +51,9 @@ def respond():
         #     warning_message = "You've used wrong url format. Please enter your url in format http://full_link or " \
         #                       "https://full_link"
         #     bot.send_message(chat_id=chat_id, text=warning_message, reply_to_message_id=msg_id)
-
-        asyncio.get_event_loop().run_until_complete(make_screenshot(url_requested[1]))
-        bot.send_message(chat_id=chat_id, text=image, reply_to_message_id=msg_id)
+        bot.send_message(chat_id=chat_id, text="I am going to make screenshot", reply_to_message_id=msg_id)
+        res = asyncio.get_event_loop().run_until_complete(make_screenshot(url_requested[1]))
+        bot.send_message(chat_id=chat_id, text=res, reply_to_message_id=msg_id)
         #bot.send_photo(chat_id=chat_id, photo=open('screen.png', 'rb'), reply_to_message_id=msg_id)
 
     else:
