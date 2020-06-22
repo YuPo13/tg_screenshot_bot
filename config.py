7c41e189a231fca9313008fe56670945d555e271
YuPo13 <yuliya.povzlo@gmail.com>
2020-06-22 22:54:30 +0300

    deploy changed from heroku to azure-4

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ PORT = int(os.environ.get('PORT', 5000))
 
 TOKEN = "1093100694:AAGKzKhSLZGX5R8aUgHe_t4wBpYdWIOsatU"
 bot_user_name = "screenshooting_bot"
-URL = "https://screenshooting.azurewebsites.net/"
+URL = "https://screensharing2.azurewebsites.net/"
